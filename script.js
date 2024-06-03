6b41c6428dc988bb851734bd5d30bde0aa772bd0
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-06-03 23:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+() => {
+    cy.visit(baseUrl + "/main.html");
+    cy.get('form').within(() => {
+        cy.get("input#name");
+        cy.get("label[for='name']");
+        cy.get("input#dob");
+        cy.get("label[for='dob']");
+        cy.get("input#email");
+        cy.get("label[for='email']");
+        cy.get("input#age");
+        cy.get("label[for='age']");
+        cy.get("input#password");
+        cy.get("label[for='password']");
+        cy.get("select#laptop");
+        cy.get("label[for='laptop']");
+        cy.get("input[name='gender']").should('have.length', 3);
+        cy.get("button[type='submit']");
+        cy.get("input[type='url']#portfolio");
+        cy.get("label[for='portfolio']");
+    });
+}
